0ae610cfff00ce0373b7b5009fc7f3cbb53ae991
Volney Tejeda <volneytejeda10@gmail.com>
2015-07-09 11:56:29 -0400

    add toggle on more-btn and menu

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var main = function() {
-
+	// when we 'click' on the '.more-btn' element...
+	$(".more-btn").on('click', function( e ) {
+		// .. prevent the lnk from taking us anywhere
+		e.preventDefault();
+		$(this).next().toggle();
+	});
+	$(".share").on('click', function() {
+		$(this).next().toggle();
+	});
+	$(".bell").click(function(){
+		$(this).find('.notification').toggleClass('active');
+	});
 };
 
 $(document).ready(main);
